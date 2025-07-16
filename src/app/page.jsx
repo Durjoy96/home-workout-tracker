2794b4fd31efd977e6f77def2967a39a721f211b
Durjoy96 <mddurjoy853@gmail.com>
2025-07-16 23:17:25 +0600

    Enhance button styles and layout in Home component; add Save Workout and Start Workout buttons

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,32 +8,38 @@ export default function Home() {
   const [exercises, setExercises] = useState([]);
 
   return (
-    <div className="bg-base-200 px-4 py-8 rounded-lg min-h-screen">
-      <h2 className="text-2xl font-bold text-base-content">
-        Create Workout Routine
-      </h2>
-      <div className="flex flex-col gap-4 mt-4">
-        {exercises.length > 0 &&
-          exercises.map((exercise) => (
-            <div
-              key={exercise.exerciseName}
-              className="flex flex-col gap-1 bg-base-300 p-4 rounded-lg"
-            >
-              <span className="text-lg font-semibold">
-                {exercise.exerciseName}
-              </span>
-              <span className="text-base text-base-content-secondary">
-                {exercise.sets} sets
-              </span>
-            </div>
-          ))}
+    <div className="bg-base-200 px-4 py-8 rounded-lg min-h-screen flex flex-col justify-between">
+      <div>
+        <h2 className="text-2xl font-bold text-base-content">
+          Create Workout Routine
+        </h2>
+        <div className="flex flex-col gap-4 mt-4">
+          {exercises.length > 0 &&
+            exercises.map((exercise) => (
+              <div
+                key={exercise.exerciseName}
+                className="flex flex-col gap-1 bg-base-300 p-4 rounded-lg"
+              >
+                <span className="text-lg font-semibold">
+                  {exercise.exerciseName}
+                </span>
+                <span className="text-base text-base-content-secondary">
+                  {exercise.sets} sets
+                </span>
+              </div>
+            ))}
+        </div>
+        <button
+          onClick={() => document.getElementById("add-exercise").showModal()}
+          className="outline-btn mt-4 flex gap-1 items-center justify-center"
+        >
+          <Plus className="w-8 hy-8 stroke-primary" /> Add Exercise
+        </button>
       </div>
-      <button
-        onClick={() => document.getElementById("add-exercise").showModal()}
-        className="outline-btn mt-4 flex gap-1 items-center justify-center"
-      >
-        <Plus className="w-8 hy-8 stroke-primary" /> Add Exercise
-      </button>
+      <div className="flex gap-4 mt-6">
+        <button className="outline-btn">Save Workout</button>
+        <button className="primary-btn">Start Workout 🔥</button>
+      </div>
       <AddExerciseModal setExercises={setExercises} />
     </div>
   );
