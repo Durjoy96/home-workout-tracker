6ddf53deedcdc08ec82a964347e84a05cf133bb7
Durjoy96 <mddurjoy853@gmail.com>
2025-07-17 21:59:19 +0600

    add exercise list toggle functionality

--- src/app/DisplayWorkouts.jsx
+++ src/app/DisplayWorkouts.jsx
@@ -3,15 +3,38 @@ import React, { useEffect, useState } from "react";
 
 export default function DisplayWorkouts() {
   const [workouts, setWorkouts] = useState([]);
+  const [displayFullList, setDisplayFullList] = useState(false);
   useEffect(() => {
     const savedWorkouts = JSON.parse(localStorage.getItem("workouts") || "[]");
     setWorkouts(savedWorkouts);
   }, []);
+  const exerciseListBtnHandler = (event) => {
+    event.stopPropagation();
+    setDisplayFullList(!displayFullList);
+  };
   return (
-    <div>
+    <div className="flex flex-col gap-4 mt-4">
       {workouts.length > 0 &&
         workouts.map((workouts, index) => (
-          <div key={index}>{workouts.name}</div>
+          <div key={index} className="bg-base-300 px-4 py-6 rounded-lg">
+            <h3 className="text-xl font-bold text-base-content">
+              {workouts.name}
+            </h3>
+            <ul className={`${displayFullList || "exercise-list"}`}>
+              {workouts.exercises.map((exercise, idx) => (
+                <li key={idx}>
+                  {exercise.exerciseName}
+                  {exercise.sets}
+                </li>
+              ))}
+            </ul>
+            {workouts.exercises.length > 3 && (
+              <button onClick={exerciseListBtnHandler}>
+                {displayFullList ? "-" : "+"} {workouts.exercises.length - 3}
+                {displayFullList ? "less" : "more"}
+              </button>
+            )}
+          </div>
         ))}
     </div>
   );
