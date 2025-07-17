d22beaad1189565058fe7e58c2b5799c96ec3be1
Durjoy96 <mddurjoy853@gmail.com>
2025-07-17 18:42:00 +0600

    Enhance WorkoutNameModal: add rest time input, save to localStorage, and redirect to home after submission

--- src/components/workout-name-modal.jsx
+++ src/components/workout-name-modal.jsx
@@ -1,20 +1,23 @@
+import { useRouter } from "next/navigation";
 import React from "react";
 
 export default function WorkoutNameModal({ setExercises, exercises }) {
+  const router = useRouter();
+
   const handleSubmit = (event) => {
     event.preventDefault(); // Prevent the default form submission
     const form = event.target;
     const workoutName = form.workoutName.value;
+    const restTime = form.restTime.value;
     console.log(workoutName);
     if (workoutName) {
-        
       const previousSavedWorkouts = JSON.parse(
         localStorage.getItem("workouts") || "[]"
       ); // Retrieve previous workouts from localStorage
 
       const NewWorkouts = [
         ...previousSavedWorkouts,
-        { name: workoutName, exercises },
+        { name: workoutName, restTime, exercises },
       ]; // Append new workout with exercises
 
       // Save the updated workouts to localStorage
@@ -23,6 +26,7 @@ export default function WorkoutNameModal({ setExercises, exercises }) {
       setExercises([]); // Clear exercises after saving
       form.reset();
       document.getElementById("save-workout").close(); // Close the modal
+      router.push("/"); // Redirect to home page
     }
   };
   return (
@@ -37,25 +41,42 @@ export default function WorkoutNameModal({ setExercises, exercises }) {
         <div>
           <form
             id="addExerciseForm"
-            className="w-full flex flex-col items-center justify-between gap-4 mt-6"
+            className="w-full mt-2"
             onSubmit={handleSubmit}
           >
-            <fieldset className="fieldset w-full">
-              <legend className="fieldset-legend">
-                Provide a Workout Name
-              </legend>
-              <input
-                type="text"
-                placeholder="eg. Full Body Workout"
-                className="p-2 w-full rounded-lg bg-base-400 focus:outline-base-400 focus:bg-base-400"
-                name="workoutName"
-                autoFocus
-                required
-              />
-            </fieldset>
+            <div>
+              <fieldset className="fieldset w-full">
+                <legend className="fieldset-legend text-base-content">
+                  Provide a Workout Name
+                </legend>
+                <input
+                  type="text"
+                  placeholder="eg. Full Body Workout"
+                  className="p-2 w-full rounded-lg bg-base-400 focus:outline-base-400 focus:bg-base-400"
+                  name="workoutName"
+                  autoFocus
+                  required
+                />
+              </fieldset>
+              <fieldset className="fieldset w-full relative">
+                <legend className="fieldset-legend text-base-content">
+                  Rest Time After Each Set
+                </legend>
+                <input
+                  type="number"
+                  placeholder="eg. 3"
+                  className="p-2 w-full rounded-lg bg-base-400 focus:outline-base-400 focus:bg-base-400"
+                  name="restTime"
+                  required
+                />
+                <span className="absolute right-6 top-1/2 -translate-y-1/2 text-base-content">
+                  min
+                </span>
+              </fieldset>
+            </div>
             <button
               type="submit"
-              className="w-full border-2 bg-primary border-primary p-2 rounded-lg cursor-pointer text-base-content"
+              className="w-full border-2 bg-primary border-primary p-2 rounded-lg cursor-pointer text-base-content mt-4"
             >
               Save
             </button>
